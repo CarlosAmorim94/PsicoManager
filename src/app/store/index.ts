efb5973ba1b752963fa542df3e4c0f118fad0ab9
carlos <carlosa@xgrow.com>
2024-04-20 11:06:53 -0300

    Feat: initial config of redux

---
+++ src/app/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState: {},
+  reducers: {},
+})
+
+export const store = configureStore({
+  reducer: {},
+})
