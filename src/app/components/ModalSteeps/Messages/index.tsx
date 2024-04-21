785f6f04f7f8ebbf5b8dd4a9555ad8ef861f6818
carlos <carlosa@xgrow.com>
2024-04-21 14:25:33 -0300

    Feat: creating messages

--- src/app/components/ModalSteeps/Messages/index.tsx
+++ src/app/components/ModalSteeps/Messages/index.tsx
@@ -1,6 +1,59 @@
 import { FC } from "react"
-import { Container } from "./styles"
+import { CustomButton } from "../../CustomButton"
+import { CustomSelect } from "../../CustomSelect"
+import { Input } from "../../Input"
+import { ButtonArea, Form } from "../UserData/styles"
+import { Container, EmailArea, Info } from "./styles"
 
 export const Messages: FC = () => {
-  return <Container>Messages</Container>
+  return (
+    <Container>
+      <h3>Enviar cobrança por e-mail:</h3>
+      <Info>
+        <p>
+          Esse é a mensagem por e-mail que seus clientes irão receber. Clique no
+          campo de texto para editar o conteúdo da mensagem e depois siga para o
+          próximo passo.
+        </p>
+      </Info>
+      <Form>
+        <EmailArea>
+          <div className="firstLine">
+            <CustomSelect
+              options={[
+                {
+                  label: "Não tem as opções no figma",
+                  value: "Não tem as opções no figma",
+                },
+              ]}
+              onChange={() => {}}
+              label="Marcação dinâmica"
+              placeholder="--Selecione--"
+            />
+            <CustomButton
+              type={"button"}
+              variant="secondary"
+              label={"Inserir"}
+              onClick={() => {}}
+            />
+          </div>
+          <Input label="Conteúdo da mensagem" isRequired />
+        </EmailArea>
+      </Form>
+      <ButtonArea>
+        <CustomButton
+          variant="secondary"
+          type={"button"}
+          onClick={() => {}}
+          label={"Cancelar"}
+        />
+        <CustomButton
+          type={"button"}
+          variant="primary"
+          label={"Próximo"}
+          onClick={() => {}}
+        />
+      </ButtonArea>
+    </Container>
+  )
 }
--- src/app/components/ModalSteeps/Messages/styles.ts
+++ src/app/components/ModalSteeps/Messages/styles.ts
@@ -1,3 +1,45 @@
 import { styled } from "styled-components"
 
-export const Container = styled.section``
+export const Container = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+  gap: 16px;
+
+  h3 {
+    width: 100%;
+    text-align: left;
+  }
+`
+
+export const EmailArea = styled.div`
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  .firstLine {
+    width: 100%;
+    display: flex;
+    align-items: end;
+    gap: 16px;
+  }
+`
+export const Info = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: #ecf5fe;
+  padding: 16px;
+  border-radius: 4px;
+
+  p {
+    color: #2196f3;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+    text-align: justified;
+  }
+`
